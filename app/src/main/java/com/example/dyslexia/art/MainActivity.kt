ae822a4b1d2fc136fe821054cb6c9311cd89e79a
DyslexiaS <exist02593@gmail.com>
2018-07-16 10:17:31 +0800

    layout

--- app/src/main/java/com/example/dyslexia/art/MainActivity.kt
+++ app/src/main/java/com/example/dyslexia/art/MainActivity.kt
@@ -5,8 +5,6 @@ import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
