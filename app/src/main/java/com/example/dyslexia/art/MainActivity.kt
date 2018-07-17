82b3678bb00d06c04e44f284facc8375760acec2
DyslexiaS <exist02593@gmail.com>
2018-07-18 02:49:19 +0800

    calendar bug

--- app/src/main/java/com/example/dyslexia/art/MainActivity.kt
+++ app/src/main/java/com/example/dyslexia/art/MainActivity.kt
@@ -5,6 +5,8 @@ import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,9 +31,26 @@ class MainActivity : AppCompatActivity() {
             startActivity(url)
         }
 
-        val phone_btn= findViewById<Button>(R.id.button5)
+        val calendar_btn = findViewById<Button>(R.id.button4)
+        calendar_btn.setOnClickListener {
+            val intent = Intent(this, MyCalendar::class.java)
+            startActivity(intent)
+        }
+
+        val phone_btn = findViewById<Button>(R.id.button5)
         phone_btn.setOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("tel:062757575#52500")))
+        }
+
+        val link_ncku = findViewById<ImageButton>(R.id.imageButton)
+        link_ncku.setOnClickListener {
+            val url = Intent(Intent.ACTION_VIEW, Uri.parse("http://web.ncku.edu.tw/"))
+            startActivity(url)
+        }
+        val link_art = findViewById<ImageButton>(R.id.imageButton2)
+        link_art.setOnClickListener {
+            val url = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.art.ncku.edu.tw/bin/home.php"))
+            startActivity(url)
         }
     }
 }
---
+++ app/src/main/java/com/example/dyslexia/art/MyCalendar.kt
@@ -0,0 +1,26 @@
+package com.example.dyslexia.art
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+
+class MyCalendar : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+
+        val calendar_ncku = findViewById<Button>(R.id.button9)
+        calendar_ncku.setOnClickListener {
+            val url = Intent(Intent.ACTION_VIEW, Uri.parse("http://web.ncku.edu.tw/p/412-1000-6149.php?Lang=zh-tw"))
+            startActivity(url)
+        }
+        val calendar_art = findViewById<Button>(R.id.button10)
+        calendar_art.setOnClickListener {
+            val url = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.art.ncku.edu.tw/files/40-1408-11-1.php?Lang=zh-tw"))
+            startActivity(url)
+        }
+    }
+}
